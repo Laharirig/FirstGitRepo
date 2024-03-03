a76bae10c1fc2b78e8c5e11d8eb7e163c7752d8b
Laharirig <161635815+Laharirig@users.noreply.github.com>
2024-03-03 23:51:18 +0530

    Add files via upload

---
+++ Beansample.java
@@ -0,0 +1,23 @@
+/*Create a Java program to use the Java Bean: Now, create another Java source file named Main.java in the same directory.*/
+// Beansample.java
+import pack.Person1;
+public class Beansample {
+    public static void main(String[] args) {
+        // Creating an instance of Person
+        Person1 person = new Person1();
+        
+        // Setting attributes
+        person.setName("John");
+        person.setAge(30);
+        
+        // Getting attributes
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());
+    }
+}
+/*Explanation:
+
+This class Samplebeas contains the main method where we create an instance of the Person class, set its attributes, and then retrieve and print them.
+Save the file: Save the file as Samplebean.java.
+
+Compile the Samplebean class: Compile Samplebean.java using the javac command.*/
